cb44f53466002873e458a7eb1b10c05cf6262849
ajteiri <ajtzjt7@gmail.com>
2026-04-26 15:49:57 +1000

    feature: added mistral into test run

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+from llama_index.llms.ollama import Ollama
 
-def execute_test_query():
+def execute_test_query(llm):
     documents = SimpleDirectoryReader("./notes.txt").load_data()
-    index = VectorStoreIndex.from_documents(documents)
+    index = VectorStoreIndex.from_documents(documents, llm=llm)
 
-    query_engine = index.as_query_engine()
+    query_engine = index.as_query_engine(llm=llm)
 
     response = query_engine.query("Summarise this content")
     print(response)
 
 def __main__():
-    execute_test_query()
+    llm = Ollama(model="mistral")
+    execute_test_query(llm)
+
+if __name__ == "__main__":
+    __main__()
