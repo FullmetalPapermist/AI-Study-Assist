e4173eb9a588066f7b38dd7f62bd062e4f1d9067
ajteiri <ajtzjt7@gmail.com>
2026-04-26 15:51:27 +1000

    fix: fixed notes according to function signature

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
 from llama_index.llms.ollama import Ollama
 
 def execute_test_query(llm):
-    documents = SimpleDirectoryReader("./notes.txt").load_data()
+    documents = SimpleDirectoryReader("./notes").load_data()
     index = VectorStoreIndex.from_documents(documents, llm=llm)
 
     query_engine = index.as_query_engine(llm=llm)
