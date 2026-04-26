5614703d629660fa9c54ca7605f06cdaf91aa028
ajteiri <ajtzjt7@gmail.com>
2026-04-26 17:12:28 +1000

    feat: added hugging face to the summary check

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
 from llama_index.llms.ollama import Ollama
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 
-def execute_test_query(llm):
+def execute_test_query(llm, embed_model):
     documents = SimpleDirectoryReader("./notes").load_data()
-    index = VectorStoreIndex.from_documents(documents, llm=llm)
 
-    query_engine = index.as_query_engine(llm=llm)
+    index = VectorStoreIndex.from_documents(
+        documents,
+        llm=llm,
+        embed_model=embed_model,
+        request_timeout=120.0
+    )
+
+    query_engine = index.as_query_engine()
 
     response = query_engine.query("Summarise this content")
     print(response)
 
 def __main__():
     llm = Ollama(model="mistral")
-    execute_test_query(llm)
+    embed_model = HuggingFaceEmbedding(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    Settings.llm = llm
+    Settings.embed_model = embed_model
+    execute_test_query(llm, embed_model)
 
 if __name__ == "__main__":
     __main__()
